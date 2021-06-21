f7fa670869a2b390ffeaaa2f4915ee288f82fba0
Shivam Rathore <shivam.rathore010@gmail.com>
2021-06-21 17:26:56 +0530

    feat: add embed test

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/Shivam010/scaling-cg
 
-go 1.14
+go 1.16
 
-require github.com/Shivam010/crispy-giggle/v2 v2.1.0
+require github.com/Shivam010/crispy-giggle/v3 v3.0.0
--- scaling.go
+++ scaling.go
@@ -1,8 +1,33 @@
 // Scaling the crispyness with the giggles of togetherness.
 package main
 
-import cg "github.com/Shivam010/crispy-giggle/v2"
+import (
+	"fmt"
+
+	cg "github.com/Shivam010/crispy-giggle/v3"
+	test "github.com/Shivam010/crispy-giggle/v3/testdata"
+)
 
 func main() {
 	cg.Main(cg.Main)
+
+	cg.License()
+	cg.LicenseDir()
+	cg.TestData()
+
+	fmt.Println("------")
+
+	fs, err := test.File.ReadDir(".")
+	fmt.Println("error", err)
+
+	fmt.Println("------")
+
+	for _, f := range fs {
+		// io.ReadAll(f)
+		fmt.Println(f.Name())
+		fmt.Println(f.IsDir())
+		d, err := test.File.ReadFile(f.Name())
+		fmt.Println(string(d), err)
+		fmt.Println()
+	}
 }
